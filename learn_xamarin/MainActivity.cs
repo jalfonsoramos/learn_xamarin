e750c90b6b2a45f0f699ed6c7b160287974361bd
Jesus Alfonso Ramos Muñiz <jesus.ramos@softtek.com>
2017-10-31 15:24:59 -0700

    add code sample

--- learn_xamarin/MainActivity.cs
+++ learn_xamarin/MainActivity.cs
@@ -13,6 +13,25 @@ namespace learn_xamarin
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user’s alphanumeric phone number to numeric
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
         }
     }
 }
